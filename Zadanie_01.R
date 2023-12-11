83a4f5bdb22f80eb4b8c842c367c9454a3913e00
pmroczek <again364@gmail.com>
2023-12-11 18:58:47 +0100

    Zadanie_02

--- Zadanie_01.R
+++ Zadanie_01.R
@@ -4,10 +4,18 @@ wyniki = c(12,26,4,24,8,11,0,13,35,16,7,24,21,17,18,6,26,31,35,18,
             31,13,19,15,10,19,23,11,27,15,31,17,22,17,33,26,35,8,14,
             6,4,27,19,21,16,4,9,7,15,30,12,9,6, 23,22,5,7,17,28,6,3)
 
-tabela_wyniki = table(wyniki)
+# Utworzenie tabeli z wartosciami  0-36 oraz zsumowanie wystąpień każdej z liczb
+tabela_wyniki <- table(factor(wyniki, levels = 0:36))
 
-tabela_wyniki
+# Przeprowadzenie testu
+test_wynik = chisq.test(tabela_wyniki)
 
-test_wynik = chisq.test(tabela_wyniki, simulate.p.value = TRUE)
 
+# Wyświetlenie wyników
 test_wynik
+
+if (test_wynik$p.value < 0.05) {
+  print("Maszyna nie jest uczciwa.")
+} else {
+  print("Maszyna jest uczciwa.")
+}
--- Zadanie_02.R
+++ Zadanie_02.R
@@ -1,22 +1,29 @@
 # Dane
-liczba_goli = c(0, 1, 2, 3, 4, 5, 6, 7)
-liczba_meczy = c(5, 7, 16, 14, 6, 3, 1, 1)
+gole = c(0, 1, 2, 3, 4, 5, 6, 7)
+mecze = c(5, 7, 16, 14, 6, 3, 1, 1)
 
-# Estymacja parametrów rozkładu Poissona
-srednia_goli = sum(liczba_goli * liczba_meczy) / sum(liczba_meczy)
+# (a) Oszacowanie parametrów rozkładu Poissona
+N = sum(mecze)
+lambda = sum(gole * mecze) / N
 
-# Teoretyczny rozkład Poissona
-teoretyczne_prawdopodobienstwa = dpois(liczba_goli, lambda = srednia_goli)
-
-# Konwersja liczb goli na wektor znaków
-liczba_goli_str = as.character(liczba_goli)
+# (b) Wygenerowanie funkcji masy prawdopodobieństwa dla rozkładu Poissona
+poisson_pmf = dpois(gole, lambda)
 
 # Wykres porównawczy
-barplot(cbind(teoretyczne_prawdopodobienstwa * sum(liczba_meczy), liczba_meczy),
-        beside = TRUE,
-        names.arg = liczba_goli_str,
-        col = c("red", "blue"),
-        legend.text = c("Teoretyczne", "Empiryczne"),
-        main = "Porównanie rozkładu empirycznego z teoretycznym",
-        xlab = "Liczba goli w meczu",
-        ylab = "Liczba meczy")
+barplot(
+  mecze / N,
+  names.arg = gole,
+  col = 'lightblue',
+  main = 'Porównanie Empiryczne vs. Rozkład Poissona',
+  xlab = 'Liczba Goli w Meczu',
+  ylab = 'Prawdopodobieństwo Empiryczne',
+  ylim = c(0, 0.4)
+)
+lines(gole, poisson_pmf, type = 'o', col = 'red')
+legend(
+  'topright',
+  legend = c('Empiryczne', 'Rozkład Poissona'),
+  col = c('lightblue', 'red'),
+  lty = 1:1,
+  cex = 0.8
+)
